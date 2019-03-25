487d18ea59fb84651008e60b111a41de949a126c
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-03-25 09:17:14 -0500

    Add project files.

---
+++ test/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    public class Car
+    {
+        public float MaxGas = 13;
+        public float GasLevel { get; set; }
+
+        public float AddGas(float gasAdded)
+        {
+            GasLevel = gasAdded + GasLevel;
+            return GasLevel;
+        }
+        public float FillUp(float gasAmount)
+        {
+            float gasAdded = MaxGas - gasAmount;
+            return gasAdded;
+        }
+        public Car(float GasLevel)
+        {
+            float gasLevel = new Random().Next(0, 13);
+            GasLevel = gasLevel;
+        }
+
+    }
+}
